daebad2288c9dd70f602ef2fb2334e421cb14759
Ole Kristian Losvik <losvik+github@gmail.com>
2019-10-01 21:31:51 +0200

    Updates

--- docs/README.md
+++
@@ -1,3 +0,0 @@
-# Documentation
-
-This is the source of documentation. 
---
+++ styles-57005dcab651d8a0a78e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{358:function(n,o,w){},965:function(n,o,w){}}]);
+//# sourceMappingURL=styles-57005dcab651d8a0a78e.js.map
